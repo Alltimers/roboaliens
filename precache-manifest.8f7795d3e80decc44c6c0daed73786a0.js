31edddcf336af9745c610e1bb06216a516220c1c
alltimers <pablowildmann@gmail.com>
2023-12-25 13:36:07 -0300

    Updates

---
+++ precache-manifest.8f7795d3e80decc44c6c0daed73786a0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23943da3a05c1a7cb865e75140abf701",
+    "url": "/roborama/index.html"
+  },
+  {
+    "revision": "53b1bcb21090e81e6e4e",
+    "url": "/roborama/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "c82da628eb43165fe039",
+    "url": "/roborama/static/css/main.b5d5b6ab.chunk.css"
+  },
+  {
+    "revision": "53b1bcb21090e81e6e4e",
+    "url": "/roborama/static/js/2.986c73ef.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/roborama/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "c82da628eb43165fe039",
+    "url": "/roborama/static/js/main.915c9254.chunk.js"
+  },
+  {
+    "revision": "1195b12eb95b7dc2d3e5",
+    "url": "/roborama/static/js/runtime~main.6c4243c8.js"
+  },
+  {
+    "revision": "c55ff0a4b73fd394622c34af1bbb2d1e",
+    "url": "/roborama/static/media/fr-bold.c55ff0a4.woff"
+  },
+  {
+    "revision": "3d084a5efa32e26700cbb1e93e7a14b3",
+    "url": "/roborama/static/media/fr-fal1.3d084a5e.woff"
+  },
+  {
+    "revision": "15e1cbb0ab440e3fea54c5adb3c6a98f",
+    "url": "/roborama/static/media/fr-fal2.15e1cbb0.woff"
+  },
+  {
+    "revision": "411ef14d6d42b776be008b68154994df",
+    "url": "/roborama/static/media/fr-title.411ef14d.woff"
+  }
+]);
